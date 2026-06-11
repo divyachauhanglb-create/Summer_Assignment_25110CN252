5ebad87cd571af15b007d6c6e10fd92a9320e7b7
Divya Chauhan <divyachauhanglb@gmail.com>
2026-06-11 18:57:34 +0530

    Commited

---
+++ Day8/Charactertriangle.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
