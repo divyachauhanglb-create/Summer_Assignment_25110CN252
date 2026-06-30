2b0e70daf53f54de4cc3d498d488dfb560750146
Divya Chauhan <divyachauhanglb@gmail.com>
2026-06-30 23:41:22 +0530

    Commited

---
+++ Day22/countwords.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+
+        String[] words = str.trim().split("\\s+");
+
+        System.out.println("Number of words = " + words.length);
+    }
+}
---
+++ Day22/removestrings.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                result += str.charAt(i);
+            }
+        }
+
+        System.out.println("String without spaces = " + result);
+    }
+}
